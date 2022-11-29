6e01395c0bb75039abef8b11c09a58fc064847a0
RokiWielki <Cruck222@interia.pl>
2022-11-29 15:34:09 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿//Napisz program, który sprawdza, czy podany rok jest rokiem przestępnym. Rok
+//przestępny dzieli się bez reszty przez 4, nie dzieli się przez 100 (za wyjątkiem lat podzielnych
+//przez 400).
+using System;
+
+namespace RokPrzestepny
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj rok: ");
+            int year = int.Parse(Console.ReadLine());
+            Rok();
+            
+
+        }
+        static bool Check(int year)
+        {
+            if (year % 4 == 0 && year % 100 != 0) return true;
+            else if (year%400==0) return true;
+            else return false;
+        }
+        static void Rok()
+        {
+            for (int i = 0; i <= 2022; i++)
+            {
+                if (Check(i) == true)
+                    Console.WriteLine(i+" Jest Przestępny");
+                else Console.WriteLine(i+ "Nie jest");
+            }
+        }
+    }
+}
